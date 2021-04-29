28b8fdd47ab2c76c931f566dc2d327fefb5073b8
a-wtnbe <ayawatanabe714@gmail.com>
2021-04-29 08:04:43 +0000

    ato7

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,7 +32,7 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
     if @book.user != current_user
-      redirect_to book_path
+      redirect_to books_path
     end
   end
 
